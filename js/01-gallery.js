c7445e63f46f490b4b8467982e62478a85d6c274
Andres7550 <119809226+Andres7550@users.noreply.github.com>
2024-02-24 00:51:13 -0500

    Actualizacion tarea 8

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,8 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-// Descrito en la documentación
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
-// Importación adicional de estilos
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 let gallery = document.querySelector('.gallery');
-
 const newImages = [...galleryItems]
-    .map(galleryItem =>`
+    .map(galleryItem => `
 <li class="gallery__item">
    <a class="gallery__link" href="${galleryItem.original}">
       <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" title="${galleryItem.description}"/>
@@ -18,9 +10,7 @@ const newImages = [...galleryItems]
 </li>`
     )
     .join('');
-
 gallery.insertAdjacentHTML('beforeend', newImages);
-
 gallery = new SimpleLightbox('.gallery a');
 gallery.on('show.simplelightbox', function () {
     gallery.options.CaptionDelay = 250;
